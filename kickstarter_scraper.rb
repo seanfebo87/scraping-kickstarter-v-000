e2f4419e4454208df2a3da883e05d3eb099c5e29
Sean Febo <seanfebo@techie.com>
2018-04-21 00:08:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,8 +2,9 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
+  html = File.read('https://www.jmbullion.com/charts/silver-prices/#')
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
